ae22badeb565bcce9044eadd397c631bf5d98c2a
Christian Oder <myself5@carbonrom.org>
2016-05-10 09:48:34 +0200

    build non-kexec support

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,6 +29,7 @@ PRODUCT_VENDOR_KERNEL_HEADERS += $(DEVICE_PATH)/kernel-headers
 TARGET_TAP_TO_WAKE_NODE := "/sys/devices/virtual/input/clearpad/wakeup_gesture"
 
 TARGET_RECOVERY_IS_MULTIROM := true
+MR_ALLOW_NKK71_NOKEXEC_WORKAROUND := true
 MR_DPI := xhdpi
 MR_DPI_FONT := 340
 MR_DEVICE_VARIANTS := z3
